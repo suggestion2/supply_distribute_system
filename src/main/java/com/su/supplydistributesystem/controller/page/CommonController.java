d2b904fae637bb1b9cbe7fbc9ffe815adc59cd69
candyforfun <liaojianyi95319@gmail.com>
2019-01-17 18:07:39 +0800

    router

--- src/main/java/com/su/supplydistributesystem/controller/page/CommonController.java
+++ src/main/java/com/su/supplydistributesystem/controller/page/CommonController.java
@@ -5,12 +5,18 @@ import com.su.supplydistributesystem.domain.User;
 import com.su.supplydistributesystem.interceptor.UserLoginRequired;
 import com.su.supplydistributesystem.request.LoginForm;
 import com.su.supplydistributesystem.request.ResetPasswordForm;
+import com.su.supplydistributesystem.response.GoodsCategoryListItemView;
+import com.su.supplydistributesystem.response.OrderItemCategoryStatisticsView;
 import com.su.supplydistributesystem.response.UserView;
+import com.su.supplydistributesystem.service.GoodsCategoryService;
 import com.su.supplydistributesystem.service.UserService;
+import com.su.supplydistributesystem.service.statistic.OrderItemDailyCount;
+import com.su.supplydistributesystem.service.statistic.StatisticsService;
 import com.sug.core.platform.crypto.MD5;
 import com.sug.core.platform.exception.ResourceNotFoundException;
 import com.sug.core.platform.web.rest.exception.InvalidRequestException;
 import com.sug.core.rest.view.ResponseView;
+import org.apache.poi.hssf.record.chart.CategorySeriesAxisRecord;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,6 +25,9 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import static com.su.supplydistributesystem.constants.CommonConstants.*;
@@ -29,6 +38,12 @@ public class CommonController {
 
     @Autowired
     private SessionContext sessionContext;
+
+    @Autowired
+    private StatisticsService statisticsService;
+
+    @Autowired
+    private GoodsCategoryService goodsCategoryService;
 
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     @UserLoginRequired
@@ -47,6 +62,29 @@ public class CommonController {
     @UserLoginRequired
     public String index(ModelMap modelMap) {
         modelMap.put("user",sessionContext.getUser());
+        modelMap.put("allOrderCount",statisticsService.countOrder(null,null,null));
+
+        List<GoodsCategoryListItemView> categoryList = goodsCategoryService.getAllListView();
+        if(Objects.nonNull(categoryList) && categoryList.size() > 0){
+            modelMap.put("category",categoryList);
+            int id = categoryList.get(0).getId();
+
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+            List<OrderItemDailyCount> list = statisticsService.countOrderCount(null,id,null,null,simpleDateFormat.format(new Date()));
+            int dailyCount = list.get(0).getCount();
+            int weeklyCount = 0;
+            for(int i =0; i < 7;i ++){
+                weeklyCount += list.get(i).getCount();
+            }
+            int monthlyCount = list.stream().mapToInt(OrderItemDailyCount::getCount).sum();
+            modelMap.put("sumOrderCount",new OrderItemCategoryStatisticsView(dailyCount,weeklyCount,monthlyCount,null));
+
+            List<OrderItemDailyCount> list1 = statisticsService.countSalesCount(null,id,null,null,simpleDateFormat.format(new Date()));
+            int weeklyCount1 = list.stream().mapToInt(OrderItemDailyCount::getCount).sum();
+            modelMap.put("sumSalesCount",new OrderItemCategoryStatisticsView(null,weeklyCount1,null,list1));
+        }
+
         return "management/index";
     }
 }
